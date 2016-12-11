5037d3efa9352b23084d142cb8088647b8241cdc
Galen <galen.scovell@gmail.com>
2016-12-10 21:19:04 -0800

    add Tiled2Unity; basic player control

--- Soulslite/Assets/scripts/PlayerController.cs
+++ Soulslite/Assets/scripts/PlayerController.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+	public float speed;
+	private Rigidbody2D rb2d;
 
-	// Use this for initialization
-	void Start () {
-		
+	// Used for initialization
+	void Start() {
+		rb2d = GetComponent<Rigidbody2D>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	// FixedUpdate is called at a fixed interval independent of frame rate -- Physics code goes here.
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+        rb2d.velocity.Set(moveHorizontal * speed, moveVertical * speed);
 	}
 }
